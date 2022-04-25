16d823f6297c86ba0205684332c0559e0c50cd87
zhaks96 <jansultan_141096@mail.ru>
2022-04-25 15:06:15 +0600

    list jobs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')" :class="{active: order === 'title'}">order by title</button>
+        <button @click="handleClick('salary')" :class="{active: order === 'salary'}">order by salary</button>
+        <button @click="handleClick('location')" :class="{active: order === 'location'}">order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobsList from './components/JobsList.vue'
+import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { JobsList },
+  setup(){
+    const jobs = ref<Job[]>([
+      {title: 'Q Test1', location: 's lon test1', salary: 1000, id: '1'},
+      {title: 'A Test2', location: 'd lon test2', salary: 3000, id: '2'},
+      {title: 'G Test3', location: 'a lon test3', salary: 2500, id: '3'},
+      {title: 'B Test4', location: 'k lon test4', salary: 2000, id: '4'},
+      {title: 'T Test5', location: 'o lon test5', salary: 5000, id: '5'},
+    ])
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  button.active, button:hover{
+    color: #d5f0ff;
+    border: 3px solid #1195c9;
+    background: #1195c9;
+    transition: all 0.5s
+  }
 </style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="job-list">
+    <p>Ordered by <span style="color: #1195c9;font-size: 18px;font-weight: bold">{{ order }}</span></p>
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} kzt</p>
+        </div>
+        <div class="description">
+          <p>Lorem test</p>
+        </div>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm'
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  },
+  setup(props){
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
+
+    return { orderedJobs }
+  }
+})
+</script>
+
+<style scoped>
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: #f2f1f175;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+
+  .list-move {
+    transition: all 1s
+  }
+</style>
